8f2708e79f1204990ec6c31f704f4f983b5a6f4c
Anshu Mittal <anshumittal200421@gmail>
2024-02-14 19:20:10 +0530

    fetch news from api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,10 +6,10 @@ export default class NewsItem extends Component {
     return (
       <div className='container my-3'>   
       <div className="card" style={{width: '18rem'}}>
-        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <img src={!imageUrl?"https://static.toiimg.com/thumb/msid-107659148,width-1070,height-580,imgsize-782647,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg":imageUrl} style={{height: '155px'}} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title.length>=45?title+'...':title}</h5>
-          <p className="card-text">{description.length>=88?description+'...':description}</p>
+          <p className="card-text">{description.length>=88?description+'...':description+"..."}</p>
           <a href= {newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
         </div>
       </div>

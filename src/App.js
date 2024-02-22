6776d7225a4472150f031b313b2309799a019ec3
Anshu Mittal <anshumittal200421@gmail>
2024-02-22 18:16:24 +0530

    Give style to source and hide api using envirment variables

--- src/App.js
+++ src/App.js
@@ -4,21 +4,36 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
+  apikey=process.env.REACT_APP_NEWS_API;
+  state = {
+    progress: 0
+  }
+
+  setProgress= (progress)=>{
+    this.setState({progress: progress});
+  }
+
   render() {
     return (
       <>
         <BrowserRouter>
-        <Navbar onSearch={this.handleSearch}/>
+        <Navbar/>
+          <LoadingBar
+            color='#f11946'
+            height={4}
+            progress={this.state.progress}
+          />
         <Routes>
-          <Route exact path='/' element={<News key="general" pageSize={9} country='in' category="general"/>}></Route>
-          <Route exact path='/business' element={<News key="business" pageSize={9} country='in' category="business"/>}></Route>
-          <Route exact path='/entertainment' element={<News key="entertainment" pageSize={9} country='in' category="entertainment"/>}></Route>
-          <Route exact path='/health' element={<News key="health" pageSize={9} country='in' category="health"/>}></Route>
-          <Route exact path='/science' element={<News key="science" pageSize={9} country='in' category="science"/>}></Route>
-          <Route exact path='/sports' element={<News key="sports"pageSize={9} country='in' category="sports"/>}></Route>
-          <Route exact path='/technology' element={<News key="technology" pageSize={9} country='in' category="technology"/>}></Route>
+          <Route exact path='/' element={<News setProgress={this.setProgress} apikey={this.apikey} key="general" pageSize={9} country='in' category="general"/>}></Route>
+          <Route exact path='/business' element={<News setProgress={this.setProgress} apikey={this.apikey}  key="business" pageSize={9} country='in' category="business"/>}></Route>
+          <Route exact path='/entertainment' element={<News setProgress={this.setProgress} apikey={this.apikey}  key="entertainment" pageSize={9} country='in' category="entertainment"/>}></Route>
+          <Route exact path='/health' element={<News setProgress={this.setProgress} apikey={this.apikey}  key="health" pageSize={9} country='in' category="health"/>}></Route>
+          <Route exact path='/science' element={<News setProgress={this.setProgress} apikey={this.apikey} key="science" pageSize={9} country='in' category="science"/>}></Route>
+          <Route exact path='/sports' element={<News setProgress={this.setProgress} apikey={this.apikey}  key="sports"pageSize={9} country='in' category="sports"/>}></Route>
+          <Route exact path='/technology' element={<News setProgress={this.setProgress} apikey={this.apikey}  key="technology" pageSize={9} country='in' category="technology"/>}></Route>
         </Routes>
         </BrowserRouter>
       </>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,8 +6,15 @@ export default class NewsItem extends Component {
     return (
       <div className='container my-3'>   
       <div className="card">
-      <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger " style={{left: "90%", zIndex: "1" }}>
-       {source?source:"Unknown"}</span>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          position: 'absolute',
+          right: 0,
+        }}>
+          <span className="badge rounded-pill bg-danger Source-badge">
+          {source?source:"Unknown"}</span>
+       </div>
         <img src={!imageUrl?"https://static.toiimg.com/thumb/msid-107659148,width-1070,height-580,imgsize-782647,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg":imageUrl} style={{height: '155px'}} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title.length>=45?title+'...':title}</h5>
